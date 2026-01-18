be3c434205f45b361cd1d6e92415eae5bcae9a59
hellocrystal <1309523341@qq.com>
2026-01-18 22:35:44 +0800

    测试Git

--- run_main.py
+++ run_main.py
@@ -35,6 +35,7 @@ def set_seed(seed):
     accelerate.utils.set_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+    # hhh
     torch.manual_seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
